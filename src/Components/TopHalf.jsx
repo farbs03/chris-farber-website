f309a06c1db5a0a635261affd287beaeac2feda6
farbs03 <cgeraldfarber@icloud.com>
2021-11-06 21:40:20 -0400

    fancy logo :)

--- src/Components/TopHalf.jsx
+++ src/Components/TopHalf.jsx
@@ -10,6 +10,7 @@ import GitHubIcon from '@material-ui/icons/GitHub';
 import InstagramIcon from '@material-ui/icons/Instagram';
 
 import profile from "./chrisfarber.jpg"
+import Logo from "./logo.png"
 
 const TopHalf = (props) => {
 
@@ -62,49 +63,63 @@ const TopHalf = (props) => {
 
   return (
     <div ref={myRef}>
-      <Grid container spacing={3} style={{color: "white", padding: "20px", justifyContent: "right"}}>
-        {links.map((link) => (
-          <Grid item>
-            <motion.div
-                initial={{y: 0, opacity: 0}}
-                animate={{opacity: 1}}
-                whileHover={{
-                    y: 1,
-                    color: "#9369db"
-                }}
-                transition={{ duration: 0.2 }}
-            >
-              <motion.a 
-                href={link.Scroll}
-                style={{
-                  textDecoration: "none",
-                  color: "inherit",
-                  fontSize: "18px",
-                }}
+      <div style={{display: "flex", alignItems: "center", justifyContent: "space-between"}}>
+        <motion.img 
+          src={Logo}
+          alt="logo"
+          style={{
+            width: "100px",
+            height: "50px",
+            padding: "5px"
+          }}
+          initial={{y: -2}}
+          animate={{y: 2}}
+          transition={{duration: 0.85, yoyo: Infinity}}
+        />
+        <Grid container spacing={3} style={{color: "white", padding: "20px", justifyContent: "right"}}>
+          {links.map((link) => (
+            <Grid item>
+              <motion.div
+                  initial={{y: 0, opacity: 0}}
+                  animate={{opacity: 1}}
+                  whileHover={{
+                      y: 1,
+                      color: "#9369db"
+                  }}
+                  transition={{ duration: 0.2 }}
               >
-                <Typography>{link.Name}</Typography>
-              </motion.a>
-            </motion.div>
-          </Grid>
-        ))}
+                <motion.a 
+                  href={link.Scroll}
+                  style={{
+                    textDecoration: "none",
+                    color: "inherit",
+                    fontSize: "18px",
+                  }}
+                >
+                  <Typography>{link.Name}</Typography>
+                </motion.a>
+              </motion.div>
+            </Grid>
+          ))}
         </Grid>
+      </div>
       <Container maxWidth="md" justifyContent="center" style={{textAlign: "center", color: "white", padding: "10px"}}>
           
           <br></br>
           <br></br>
           <motion.div 
-            style={{width: "200px", height: "200px", borderRadius: "50%", background:"white", marginLeft: "auto", marginRight: "auto", border: "2px solid"}} 
+            style={{width: "200px", height: "200px", borderRadius: "50%", marginLeft: "auto", marginRight: "auto", padding: '2px',  background: "linear-gradient(90deg, rgba(0,255,249,1) 0%, rgba(29,82,253,1) 50%, rgba(147,105,219,1) 100%)"}} 
             initial={{opacity: 0}} 
             animate={{opacity: 1}} 
             whileHover={{scale: 1.02}} 
             transition={{duration: 0.2}}
           >
               <motion.img 
+                src={profile} 
+                style={{width: "100%", height: "100%", borderRadius: "50%"}} 
                 initial={{opacity: 0}} 
                 animate={{opacity: 1}}
                 transition={{duration: 0.2}}
-                src={profile} 
-                style={{width: "100%", height: "100%", borderRadius: "50%"}} 
                 alt="profile-pic"
               />
           </motion.div>
@@ -118,22 +133,21 @@ const TopHalf = (props) => {
           {titles.map((title, idx) => (
               <Fade in mountOnEnter unmountOnExit style={{transitionDelay: `${200 + 100 * idx}ms`, transitionDuration: "1000ms"}}>
                   <div style={{display: "inline-block"}}>
-                  <Typography style={{margin: "15px", fontSize: "20px", borderBottom: "2px solid"}}>{title}</Typography>
+                    <Typography style={{margin: "15px", fontSize: "20px", borderBottom: "2px solid"}}>{title}</Typography>
                   </div>
               </Fade>
           ))}
           <br></br>
           <br></br>
-          <div style={{width: "280px", margin: "0px auto", textAlign: 'center'}}>
+          <div style={{display: "flex", margin: "0px auto", textAlign: 'center', justifyContent: "center", alignItems: "center"}}>
               {contactLinks.map((contact) => (
                   <a href={contact.Link} target="_blank">
                       <motion.div 
                           style={{
                               width: "50px", 
                               height: "50px", 
-                              display: "inline-block", 
                               margin: "0px 8px", 
-                              color: "#9369db", 
+                              color: "#9369db",
                               borderRadius: "50%", 
                               border: "2px solid #9369db"
                           }}
